979ae746dba349b6ce48a67469c103e2a16a8f88
arizvi-prog <alishajrizvi@gmail.com>
2024-07-29 13:12:07 -0400

    finished basic student app api

--- db/connection.js
+++ db/connection.js
@@ -1,30 +1,30 @@
-// const { Pool } = require('pg');
-// const is_prod = process.env.NODE_ENV === 'production';
-
-// const localConnection = {
-//   user: 'postgres',
-//   password: 'pass',
-//   database: 'student_app_db'
-// };
-
-// const renderConnection = {
-//   connectionString: process.env.DB_URL,
-//   ssl: {
-//     rejectUnauthorized: false, // Set to false to bypass SSL certificate validation (not recommended for production)
-//   },
-// }
-
-// const connectObj = is_prod ? renderConnection : localConnection;
-
-// const client = new Pool(connectObj);
-
-// module.exports = client;
-
 const { Pool } = require('pg');
-const client = new Pool({
+const is_prod = process.env.NODE_ENV === 'production';
+
+const localConnection = {
   user: 'postgres',
   password: 'pass',
   database: 'student_app_db'
-});
+};
+
+const renderConnection = {
+  connectionString: process.env.DB_URL,
+  ssl: {
+    rejectUnauthorized: false, // Set to false to bypass SSL certificate validation (not recommended for production)
+  },
+}
+
+const connectObj = is_prod ? renderConnection : localConnection;
+
+const client = new Pool(connectObj);
 
 module.exports = client;
+
+// const { Pool } = require('pg');
+// const client = new Pool({
+//   user: 'postgres',
+//   password: 'pass',
+//   database: 'student_app_db'
+// });
+
+// module.exports = client;
